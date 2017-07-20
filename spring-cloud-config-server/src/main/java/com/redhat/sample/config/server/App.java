440b92f9343e95affac005e0670b882b4147a109
rhwen <wangwh8@gmail.com>
2017-07-20 11:31:41 +0800

    initial config server project.

---
+++ spring-cloud-config-server/src/main/java/com/redhat/sample/config/server/App.java
@@ -0,0 +1,13 @@
+package com.redhat.sample.config.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
