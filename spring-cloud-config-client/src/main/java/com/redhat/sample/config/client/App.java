8ef2d4766dc2cd7984bcf4458156b023595bd608
rhwen <wangwh8@gmail.com>
2017-07-20 11:35:26 +0800

    initial config client project

---
+++ spring-cloud-config-client/src/main/java/com/redhat/sample/config/client/App.java
@@ -0,0 +1,13 @@
+package com.redhat.sample.config.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
